64f10d40f40c3a435350d614656de0f9fbff1d53
顾然 <gu.ran@asiainfo-sec.com>
2026-08-31 14:55:52 +0800

    chore: 删除死代码(WasteCode、getLabelSpecOptions、未用import)及仓库残留文件

--- trace-backend/src/main/java/com/huace/trace/mapper/WasteCodeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.huace.trace.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.huace.trace.entity.WasteCode;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface WasteCodeMapper extends BaseMapper<WasteCode> {
-}
