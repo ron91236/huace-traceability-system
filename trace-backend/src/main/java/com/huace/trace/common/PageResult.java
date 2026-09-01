df863f439fb0678141391b1d670004d8cf76676f
顾然 <gu.ran@asiainfo-sec.com>
2026-09-01 11:43:30 +0800

    fix: PageResult增加@JsonCreator构造器以支持Redis缓存反序列化
    缓存命中时Jackson无法实例化PageResult（缺少默认构造器），为构造器标注@JsonCreator/@JsonProperty

--- trace-backend/src/main/java/com/huace/trace/common/PageResult.java
+++ trace-backend/src/main/java/com/huace/trace/common/PageResult.java
@@ -1,5 +1,7 @@
 package com.huace.trace.common;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 
@@ -8,7 +10,8 @@ public class PageResult<T> {
     private List<T> list;
     private long total;
 
-    public PageResult(List<T> list, long total) {
+    @JsonCreator
+    public PageResult(@JsonProperty("list") List<T> list, @JsonProperty("total") long total) {
         this.list = list;
         this.total = total;
     }
